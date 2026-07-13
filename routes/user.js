ce5b94a68f2d02aafcccf1bbd756135b0db33d07
devanshuR10 <devanshur583@gmail.com>
2026-07-13 12:34:57 +0530

    Add SSL configuration to database connection

--- routes/user.js
+++ routes/user.js
@@ -9,6 +9,9 @@ const pool = new Pool({
     user: process.env.PGUSER,
     password: process.env.PGPASSWORD,
     database: process.env.PGDATABASE,
+    ssl: {
+        rejectUnauthorized: false,
+    },
 });
 
 
